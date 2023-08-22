d8531059d39393da00666748b6be18dbd35fa974
cnkelvin <contato.kelvinnielson@gmail.com>
2023-08-22 00:15:19 -0300

    adjusting my commit errors

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,7 +1,11 @@
 import './style.css'
 
+export type CardProps = {
+	name: string;
+	time: string;
+};
 
-export function Card(props){
+export function Card(props: CardProps){
 	return(
 		<div className="card">
 			<strong>{props.name}</strong>
